405b8b539d5de32112a6608df685a05b93d95222
ajlopez <webmaster@ajlopez.com>
2013-07-21 10:41:29 -0300

    Application create with options

--- test/application.js
+++ test/application.js
@@ -8,3 +8,10 @@ var app = ajforce.createApplication('Warehouse');
 assert.ok(app);
 assert.equal(app.Name, 'Warehouse');
 
+// create application with options
+
+var app = ajforce.createApplication('Warehouse', { Description: 'Warehouse Application', Author: 'me' });
+assert.ok(app);
+assert.equal(app.Name, 'Warehouse');
+assert.equal(app.Description, 'Warehouse Application');
+assert.equal(app.Author, 'me');
