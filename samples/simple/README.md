75c303547b1efbff3c3c0575150f9f6f4ce6d0c3
ajlopez <webmaster@ajlopez.com>
2013-08-22 18:58:04 -0300

    simple web site, WIP

---
+++ samples/simple/README.md
@@ -0,0 +1,31 @@
+# My Company
+
+Simple index page with:
+
+- Express
+- MongoDb
+- Ejs for views
+- Ejs-locals for view layout
+- Twitter Bootstrap for styles
+
+## Install
+
+```
+npm install
+```
+
+## Run
+
+```
+node app
+```
+Locally starts as `http://localhost:3000`.
+
+## References
+
+- [The Node.JS MongoDB Driver Manual](http://mongodb.github.io/node-mongodb-native/)
+- [Twitter Bootstrap](http://twitter.github.io/bootstrap/)
+- [Twitter Bootstrap Starter Template](http://twitter.github.io/bootstrap/examples/starter-template.html)- [ejs-locals](https://github.com/RandomEtc/ejs-locals)
+- [Express template demo](https://github.com/chovy/express-template-demo)
+- [Layouts in Express 3 and EJS](http://stackoverflow.com/questions/12616694/layouts-in-express-3-and-ejs)
+- [Render ejs with express](http://runnable.com/UTlPPF-f2W1TAAEa/render-ejs-with-express-node-js)
---
+++ samples/simple/app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+//  , customers = require('./routes/customers')
+  , engine = require('ejs-locals')
+  , http = require('http')
+  , path = require('path');
+  
+// var db = mongorepo.openDatabase('mycompany', 'localhost', 27017);
+// customers.initialize(db); 
+
+var app = express();
+
+app.engine('ejs', engine);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+/*
+app.get('/customers', customers.index);
+app.get('/customers/new', customers.create);
+app.post('/customers/new', customers.insert);
+app.get('/customers/:id', customers.view);
+app.get('/customers/:id/edit', customers.edit);
+app.post('/customers/:id/edit', customers.update);
+app.get('/customers/:id/remove', customers.remove);
+*/
+
+app.get('/about', function (req, res) { res.render('index', { title: 'About' }); });
+app.get('/contact', function (req, res) { res.render('index', { title: 'Contact' }); });
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
